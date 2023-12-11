b6088b86aafeb22c65b7a13eb121bd5d2762cd30
olkres <belirium29@gmail.com>
2023-12-11 22:41:37 +0200

    feat: make burger menu for 768 and 380

--- coffee-house/pages/home/index.js
+++ coffee-house/pages/home/index.js
@@ -1,1 +1,23 @@
+// BURGER
+(function () {
+    const burger = document.querySelector('.burger-menu');
+    const menu = document.querySelector('.nav');
+
+    const body = document.body;
+
+    
+     burger.addEventListener('click', () => {
+         burger.classList.toggle('active');   
+         menu.classList.toggle('nav-active');
+         body.classList.toggle('no-scroll');
+         
+     });
+     menu.querySelectorAll('.nav-link').forEach(link => {
+		link.addEventListener('click', () => {
+			burger.classList.remove('active')
+			menu.classList.remove('nav-active')
+			body.classList.remove('no-scroll')
+		})
+	})
+ }());
 alert('Доброго времени суток, проверяющий. Прошу подождать с проверкой.\nДо четверга постараюсь сделать. Спасибо.');
